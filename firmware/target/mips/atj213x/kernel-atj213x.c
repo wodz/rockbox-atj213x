6d2c3b68b351bfd48e02a584efd0883f31da3574
Marcin Bukat <marcin.bukat@gmail.com>
2014-12-18 12:28:53 +0100

    ATJ213x: timer driver

---
+++ firmware/target/mips/atj213x/kernel-atj213x.c
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 Marcin Bukat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include "config.h"
+#include "system.h"
+#include "kernel.h"
+#include "rtctmr-atj213x.h"
+
+void tick_start(unsigned int interval_in_ms)
+{
+    atj213x_timer_set(TIMER_TICK, interval_in_ms, call_tick_tasks);
+    atj213x_timer_start(TIMER_TICK);
+}
