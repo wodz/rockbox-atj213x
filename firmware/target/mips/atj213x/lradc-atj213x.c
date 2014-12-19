5d0701f935aa35b7eb60d0235687bf67d24df6bc
Marcin Bukat <marcin.bukat@gmail.com>
2014-12-19 11:53:43 +0100

    ATJ213x lradc driver

---
+++ firmware/target/mips/atj213x/lradc-atj213x.c
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Marcin Bukat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+ 
+#include "config.h"
+#include "system.h"
+#include "gpio-atj213x.h"
+
+void lradc_init(void)
+{
+    PMU_CTL |= BF_PMU_CTL_LA6E(1)|BF_PMU_CTL_LA4E(1);
+}
+
+unsigned int lradc_read(unsigned channel)
+{
+    switch (channel)
+    {
+        case LRADC_CH_KEY:
+            return (PMU_LRADC & BM_PMU_LRADC_REMOADC4)>>BP_PMU_LRADC_REMOADC4;
+            break;
+
+        case LRADC_CH_BAT:
+            return (PMU_LRADC & BM_PMU_LRADC_BATADC6)>>BP_PMU_LRADC_BATADC6;
+            break;
+
+        case LRADC_CH_TEMP:
+            return (PMU_LRADC & BM_PMU_LRADC_TEMPADC6)>>BP_PMU_LRADC_TEMPADC6;
+            break;
+
+        default:
+            panicf("lradc_read() invalid channel");
+    }
+
+    return -1;
+}
---
+++ firmware/target/mips/atj213x/lradc-atj213x.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2014 by Marcin Bukat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+ 
+#include "config.h"
+#include "system.h"
+#include "atj213x.h"
+
+enum {
+    LRADC_CH_KEY,
+    LRADC_CH_BAT,
+    LRADC_CH_TEMP
+};
+
+void lradc_init(void);
+unsigned int lradc_read(unsigned channel);
