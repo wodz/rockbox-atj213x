9713adf35802ae38c674145ccd2984d3991e18a9
Marcin Bukat <marcin.bukat@gmail.com>
2015-01-07 10:04:56 +0100

    e150 buttons WIP

---
+++ firmware/target/mips/atj213x/iriver-e150/button-e150.c
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2015 Marcin Bukat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+#include "config.h"
+#include "cpu.h"
+#include "system.h"
+#include "button.h"
+
+bool button_hold(void)
+{
+    return !atj213x_gpio_get(PORTA, 10);
+}
+
+void button_init_device(void)
+{
+    /* setup button gpios as input */
+    
+}
+
+/*
+ * Get button pressed from hardware
+ */
+int button_read_device(void)
+{
+}
