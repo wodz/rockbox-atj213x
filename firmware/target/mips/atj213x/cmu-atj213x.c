3eaf6b601343dea74f5e3022c271f579222b3210
Marcin Bukat <marcin.bukat@gmail.com>
2016-02-17 15:28:02 +0100

    atj213x: Add helper functions for clock gating

---
+++ firmware/target/mips/atj213x/cmu-atj213x.c
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2016 by Marcin Bukat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+ 
+#include "cmu-atj213x.h"
+#include "regs/regs-cmu.h"
+
+void atj213x_clk_enable(unsigned int blockno)
+{
+    CMU_DEVCLKEN |= (1<<clkno);
+    udelay(1);
+}
+
+void atj213x_clk_disable(unsigned int blockno)
+{
+    CMU_DEVCLKEN &= ~(1<<clkno);
+    udelay(1);
+}
+
+void atj213x_block_reset(unsigned int blockno)
+{
+    CMU_DEVRST &= ~(1<<blockno);
+    udelay(10);
+    CMU_DEVRST |= (1<<blockno);
+    udelay(10);
+}
---
+++ firmware/target/mips/atj213x/cmu-atj213x.h
@@ -0,0 +1,26 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2016 by Marcin Bukat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef INTC_ATJ213X_H
+#define INTC_ATJ213X_H
+ 
+void atj213x_clk_enable(unsigned int blockno);
+void atj213x_clk_disable(unsigned int blockno);
+void atj213x_block_reset(unsigned int blockno);
+#endif /* INTC_ATJ213X_H */
